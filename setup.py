b906d1ad6bcd255f82c1fed838dcbdc806f10a56
Peter Friedrich <peter.friedrich@mail.de>
2016-05-02 10:29:56 +0200

    added packaging files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+
+setup(name='tgo',
+      version='0.1',
+      description='Implementation of the topographical global optimisationalgorithm',
+      url='https://github.com/stefan-endres/tgo',
+      include_package_data=True,
+      packages=['tgo'])
